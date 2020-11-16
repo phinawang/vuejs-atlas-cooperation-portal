208c3f765d9c0bc81c5b184ef609e8b17b7b7f92
Phina <phiphiphina@gmail.com>
2020-11-16 23:48:12 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./styles/index.css";
+import VueRouter from "vue-router";
+import PageNotFound from "./components/Page404.vue";
+import audit from "./components/Audit.vue";
+import usage from "./components/Usage.vue";
 
 Vue.config.productionTip = false;
 
+const routes = [
+  { path: "/audit", component: audit },
+  { path: "/usage", component: usage },
+  { path: "*", component: PageNotFound }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
+
 new Vue({
+  router,
   render: (h) => h(App)
 }).$mount("#app");
